4701862ae6cb0823cbff943512379f35aa3331ae
amin <aminledragon@gmail.com>
2024-01-23 16:52:14 +0100

    last updated project

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -7,11 +7,22 @@ import Plan from "../Plan/Plan";
 export default function Display({ data }) {
   const [step1, setStep1] = useState(true);
   const [step2, setStep2] = useState(false);
+  const [time, setTime] = useState("monthly");
+  const [miniCard, setMiniCard] = useState("");
+
   return (
     <div className="Display">
       <SideBar data={data} />
       {step1 ? <Info setStep1={setStep1} setStep2={setStep2} /> : null}
-      {step2 ? <Plan setStep2={setStep2} /> : null}
+      {step2 ? (
+        <Plan
+          setTime={setTime}
+          time={time}
+          miniCard={miniCard}
+          setMiniCard={setMiniCard}
+          setStep2={setStep2}
+        />
+      ) : null}
     </div>
   );
 }
--- src/Components/Plan/Plan.jsx
+++ src/Components/Plan/Plan.jsx
@@ -1,13 +1,60 @@
 import React from "react";
 import "./Plan.css";
+import arcade from "../../img/arcade.svg";
+import advenced from "../../img/advenced.svg";
+import pro from "../../img/pro.svg";
 
-export default function Plan() {
+export default function Plan({ setMiniCard, miniCard }) {
+  const data = [
+    {
+      id: 1,
+      image: arcade,
+      title: "Arcade",
+      price: 9,
+    },
+    {
+      id: 2,
+      image: advenced,
+      title: "Advanced",
+      price: 12,
+    },
+    {
+      id: 3,
+      image: pro,
+      title: "Pro",
+      price: 15,
+    },
+  ];
+
   return (
     <div className="Plan">
       <div className="top">
         <h1>Select your plan</h1>
         <p>you have the option of monthlt or yearly billing.</p>
       </div>
+      <div className="cards">
+        {data.map((element, index) => {
+          return (
+            <div
+              key={index}
+              className={`card ${
+                miniCard === element.title ? "effetCard" : ""
+              }`}
+              onClick={() => setMiniCard(element.title)}
+            >
+              <img src={element.image} alt={element.image} />
+              <div className="info">
+                <h4> {element.title} </h4>
+                <div className="price">
+                  <p>${element.price}</p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="switch"></div>
     </div>
   );
 }
