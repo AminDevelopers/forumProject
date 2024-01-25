69b4977d0b055c33f615e71a1fcc423ed908ca9d
amin <aminledragon@gmail.com>
2024-01-25 15:34:25 +0100

    last update + style basics for summary

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import "./Summary.css";
 
-export default function Summary() {
-  return <div className="Summary"></div>;
+export default function Summary({ currentStep, setCurrentStep }) {
+  const changePlus = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+  const changeMines = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  return (
+    <div className="Summary">
+      <div className="topSummary">
+        <h1>Finishing up </h1>
+        <p>Double check everything looks OK before confirming.</p>
+      </div>
+      <div className="finish"></div>
+      <div className="btn">
+        <button id="firstBtnd" onClick={changeMines}>
+          go back
+        </button>
+        <button id="secondBtnSummary" onClick={changePlus}>
+          confirm
+        </button>
+      </div>
+    </div>
+  );
 }
